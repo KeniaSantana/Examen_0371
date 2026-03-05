72bd2f21211d83f773490ea740212ceb023fbd65
Kenia <23308060610371@cetis61.edu.mx>
2026-03-05 13:24:03 -0500

    x

--- ficha/main.py
+++ ficha/main.py
@@ -62,6 +62,51 @@ def main(page: ft.Page):
         size=15,
         color=ft.Colors.RED,
     )
+    
+    final = ft.Text("Gracias por tu registro",
+                    size=30, 
+                    weight=ft.FontWeight.BOLD,
+                        color=ft.Colors.BLUE,
+                    text_align=ft.TextAlign.CENTER)
+    
+    
+    def mostrar_resumen(e):
 
+        texto = f"""
+Nombre: {nombre.value}
+Correo:{correo.value}
+Taller: {taller.value}
+Modalidad de Pago: {modalidad_pago.value}
+Nivel de Experiencia:{deslizador.value}
+"""
+
+        resumen.value = texto
+        resumen.color = ft.Colors.RED
+
+        lista_eventos.controls.append(
+            ft.Text(texto)
+        )
+        
+    boton = ft.ElevatedButton(
+        "Registrar",
+        on_click=mostrar_resumen,
+        bgcolor=ft.Colors.BLUE,
+        color=ft.Colors.WHITE
+    )
+
+    page.add(
+        titulo,
+        nombre,
+        correo,
+        modalidad_pago,
+        casilla,
+        deslizador,
+        final,
+        boton,
+        ft.Divider(),
+        resumen,
+        ft.Text("Eventos guardados:"),
+        lista_eventos
+    )
 
 ft.run(main)
